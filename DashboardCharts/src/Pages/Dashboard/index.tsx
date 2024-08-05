74cf628cc77f94a024df7da96609d4b22d052e11
cynthia-isutsa <isutsacynthia@gmail.com>
2024-08-05 11:16:40 +0300

    feat: [ant design static cards ]

--- DashboardCharts/src/Pages/Dashboard/index.tsx
+++ DashboardCharts/src/Pages/Dashboard/index.tsx
@@ -1,18 +1,21 @@
 import {
   DollarCircleOutlined,
+  EllipsisOutlined,
   ShoppingCartOutlined,
   ShoppingOutlined,
   UserOutlined,
 } from "@ant-design/icons";
-import { PageContainer, ProTable } from "@ant-design/pro-components";
-import { Card, Space, Statistic, Typography } from "antd";
+import { PageContainer, ProTable, StatisticCard } from "@ant-design/pro-components";
+import { Card, Divider, Space, Statistic, Typography } from "antd";
 import { useEffect, useState } from "react";
 import { getRecentOrders } from "../API";
+import RcResizeObserver from 'rc-resize-observer';
 
 
 const index = () => {
  const [dataSource, setDataSource] = useState<any>([])
  const [loading, setLoading] = useState<any>(false)
+ const [responsive, setResponsive] = useState(false);
 
  useEffect(() => {
   setLoading(true)
@@ -100,7 +103,97 @@ const index = () => {
             </Space>
           </Card>
         </Space>
-        <br />
+        <Divider />
+        {/* <StatisticCard
+      title={
+        <Space>
+          <span> Ant Design Graph</span>
+          <RightOutlined style={{ color: "lightblue" }} />
+        </Space>
+      }
+      extra={<EllipsisOutlined />}
+      statistic={{
+        value: 1102893,
+        prefix: 'Ksh.',
+        description: (
+          <Space>
+            <Statistic title="Percentage" value="82.3%" />
+            <Statistic title="Amount" value="6000" />
+          </Space>
+        ),
+      }}
+      chart={
+        <>
+          <img
+            src="https://gw.alipayobjects.com/zos/alicdn/BA_R9SIAV/charts.svg"
+            alt="chart"
+            width="100%"
+          />
+        </>
+      }
+      style={{ width: 268 }}
+    /> */}
+     <StatisticCard
+      title="Example of Antd chart"
+      tooltip="Sales"
+      style={{ maxWidth: 480 }}
+      extra={<EllipsisOutlined />}
+      chart={
+        <img
+          src="https://gw.alipayobjects.com/zos/alicdn/a-LN9RTYq/zhuzhuangtu.svg"
+          alt="sales chart"
+          width="100%"
+        />
+      }
+      />
+       <RcResizeObserver
+      key="resize-observer"
+      onResize={(offset) => {
+        setResponsive(offset.width < 596);
+      }}
+    >
+      <StatisticCard.Group direction={responsive ? 'column' : 'row'}>
+        <StatisticCard
+          statistic={{
+            title: '总流量(人次)',
+            value: 601986875,
+          }}
+        />
+        <Divider type={responsive ? 'horizontal' : 'vertical'} />
+        <StatisticCard
+          statistic={{
+            title: '付费流量',
+            value: 3701928,
+            description: <Statistic title="占比" value="61.5%" />,
+          }}
+          chart={
+            <img
+              src="https://gw.alipayobjects.com/zos/alicdn/ShNDpDTik/huan.svg"
+              alt="百分比"
+              width="100%"
+            />
+          }
+          chartPlacement="left"
+        />
+        <StatisticCard
+          statistic={{
+            title: '免费流量',
+            value: 1806062,
+            description: <Statistic title="占比" value="38.5%" />,
+          }}
+          chart={
+            <img
+              src="https://gw.alipayobjects.com/zos/alicdn/6YR18tCxJ/huanlv.svg"
+              alt="百分比"
+              width="100%"
+            />
+          }
+          chartPlacement="left"
+        />
+      </StatisticCard.Group>
+    </RcResizeObserver>
+    
+        <Divider />
         <Space direction="vertical">
           <Typography.Title level={2}>Recent Orders</Typography.Title>
           <ProTable
