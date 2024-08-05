2c0989e6d22d6beb8c483c3b395ef6c08dbfb7d5
cynthia-isutsa <isutsacynthia@gmail.com>
2024-08-05 05:27:59 +0300

    refactor: [adding data table on dasboard table]

--- DashboardCharts/src/Pages/Dashboard/index.tsx
+++ DashboardCharts/src/Pages/Dashboard/index.tsx
@@ -4,10 +4,26 @@ import {
   ShoppingOutlined,
   UserOutlined,
 } from "@ant-design/icons";
-import { PageContainer } from "@ant-design/pro-components";
+import { PageContainer, ProTable } from "@ant-design/pro-components";
 import { Card, Space, Statistic, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { getRecentOrders } from "../API";
+
 
 const index = () => {
+ const [dataSource, setDataSource] = useState<any>([])
+ const [loading, setLoading] = useState<any>(false)
+
+ useEffect(() => {
+  setLoading(true)
+  getRecentOrders().then(res =>{
+    console.log({res})
+    setDataSource(res?.products)
+    setLoading(false)
+  })
+ }, [])
+
+
   return (
     <div>
       <PageContainer
@@ -25,6 +41,7 @@ const index = () => {
           ),
         }}
       >
+        
         <Space direction="horizontal">
           <Card>
             <Space direction="horizontal">
@@ -42,15 +59,14 @@ const index = () => {
           </Card>
           <Card>
             <Space direction="horizontal">
-              <ShoppingOutlined 
-               style={{
-                color: "purple",
-                backgroundColor: "rgba(0, 255,255,0.25)",
-                borderRadius: 20,
-                padding: 8,
-                fontSize: 24,
-              }}
-              
+              <ShoppingOutlined
+                style={{
+                  color: "purple",
+                  backgroundColor: "rgba(0, 255,255,0.25)",
+                  borderRadius: 20,
+                  padding: 8,
+                  fontSize: 24,
+                }}
               />
               <Statistic title="Inventory" value={1236} />
             </Space>
@@ -64,29 +80,93 @@ const index = () => {
                   borderRadius: 20,
                   padding: 8,
                   fontSize: 24,
-                }} 
-                />
+                }}
+              />
               <Statistic title="Customer" value={34576} />
             </Space>
           </Card>
           <Card>
             <Space direction="horizontal">
-              <DollarCircleOutlined 
-              style={{
-                color: "red",
-                backgroundColor: "rgba( 255,0, 0,0.25)",
+              <DollarCircleOutlined
+                style={{
+                  color: "red",
+                  backgroundColor: "rgba( 255,0, 0,0.25)",
                   borderRadius: 20,
                   padding: 8,
                   fontSize: 24,
                 }}
-                 />
+              />
               <Statistic title="Revenue" value={1236000} />
             </Space>
           </Card>
+        </Space>
+        <br />
+        <Space direction="vertical">
+          <Typography.Title level={2}>Recent Orders</Typography.Title>
+          <ProTable
+          search={false}
+          dataSource={dataSource}
+          options={false}
+          loading={loading}
+            columns={[
+              {
+                title: "Id",
+                dataIndex: "id",
+                key: "id",
+                width: 150,
+                
+              },
+              {
+                title: "Product",
+                dataIndex: "title",
+                key: "product",
+                width: 350,
+              },
+              {
+                title: "Price",
+                dataIndex: "price",
+                key: "price",
+                width: 150,
+                valueType: {
+                  type: "money",
+                  moneySymbol: false
+                }
+              },
+              {
+                title: "Quantity",
+                dataIndex: "quantity",
+                key: "quantity",
+                width: 150,
+              },
+              {
+                title: "Total",
+                dataIndex: "total",
+                key: "total",
+                width: 150,
+                valueType: {
+                  type: "money",
+                  moneySymbol: false
+                }
+              },
+              {
+                title: "Discounted Total",
+                dataIndex: "discountedTotal",
+                key: "discounted",
+                width: 150,
+                valueType: {
+                  type: "money",
+                  moneySymbol: false
+                }
+              },
+            ]}
+            pagination={false}
+          />
         </Space>
       </PageContainer>
     </div>
   );
 };
+
+
 
 export default index;
